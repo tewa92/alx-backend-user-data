28a9a72e93eb00e23805992149dc3931acc88f9e
tewa92 <tewachewsaleigizer@gmail.com>
2024-09-13 11:34:56 -0700

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -116,5 +116,4 @@ class Auth:
         self._db.update_user(
             user.id,
             hashed_password=new_password_hash,
-            reset_token=None,
-        )
+            reset_token=None,)
